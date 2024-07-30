079add9f76846aee2d8d32a520c0400b87e8dee0
Sameer Ahmed <102585406+Mojojojono@users.noreply.github.com>
2024-07-30 16:32:16 +0500

    disigning the video page to display required things

--- src/Components/Play Video/PlayVideo.jsx
+++ src/Components/Play Video/PlayVideo.jsx
@@ -35,7 +35,126 @@ const PlayVideo = () => {
         </div>
       </div>
       <hr />
-      <div className="publisher "></div>
+      <div className="publisher">
+        <img src={jack} alt="" />
+        <div>
+          <p>MojoJojo</p>
+          <span>1B Subscribers</span>
+        </div>
+        <button>Subscribe</button>
+      </div>
+      <div className="vid-desc">
+        <p>Lets learn reactjs</p>
+        <p>
+          {" "}
+          Learning to code because believe that oneday it will work for me IA
+          Learning to code because believe that oneday it will work for me IA
+          Learning to code because believe that oneday it will work for me IA
+          Learning to code because believe that oneday it will work for me IA
+        </p>
+        <hr />
+        <h4>130M comments</h4>
+        <div className="comment">
+          <img src={user_profile} alt="" />
+          <div>
+            <h3>
+              Sameer <span>12 years ago</span>
+            </h3>
+            <p>
+              Learning to code because believe that oneday it will work for me
+              IA Learning to code because believe that oneday it will work for
+              me IA Learning to code because believe that oneday it will work
+              for me IA Learning to code because believe that oneday it will
+              work for me IA Learning to code because believe that oneday it
+              will work for me IA Learning to code because believe that oneday
+              it will work for me IA Learning to code because believe that
+              oneday it will work for me IA Learning to code because believe
+              that oneday it will work for me IA
+            </p>
+            <div className="comment-action">
+              <img src={like} alt="" />
+              <span>14984</span>
+              <img src={dislike} alt="" />
+              <span>62368</span>
+            </div>
+          </div>
+        </div>
+        <div className="comment">
+          <img src={user_profile} alt="" />
+          <div>
+            <h3>
+              Sameer <span>12 years ago</span>
+            </h3>
+            <p>
+              Learning to code because believe that oneday it will work for me
+              IA Learning to code because believe that oneday it will work for
+              me IA Learning to code because believe that oneday it will work
+              for me IA Learning to code because believe that oneday it will
+              work for me IA Learning to code because believe that oneday it
+              will work for me IA Learning to code because believe that oneday
+              it will work for me IA Learning to code because believe that
+              oneday it will work for me IA Learning to code because believe
+              that oneday it will work for me IA
+            </p>
+            <div className="comment-action">
+              <img src={like} alt="" />
+              <span>14984</span>
+              <img src={dislike} alt="" />
+              <span>62368</span>
+            </div>
+          </div>
+        </div>
+        <div className="comment">
+          <img src={user_profile} alt="" />
+          <div>
+            <h3>
+              Sameer <span>12 years ago</span>
+            </h3>
+            <p>
+              Learning to code because believe that oneday it will work for me
+              IA Learning to code because believe that oneday it will work for
+              me IA Learning to code because believe that oneday it will work
+              for me IA Learning to code because believe that oneday it will
+              work for me IA Learning to code because believe that oneday it
+              will work for me IA Learning to code because believe that oneday
+              it will work for me IA Learning to code because believe that
+              oneday it will work for me IA Learning to code because believe
+              that oneday it will work for me IA
+            </p>
+            <div className="comment-action">
+              <img src={like} alt="" />
+              <span>14984</span>
+              <img src={dislike} alt="" />
+              <span>62368</span>
+            </div>
+          </div>
+        </div>
+        <div className="comment">
+          <img src={user_profile} alt="" />
+          <div>
+            <h3>
+              Sameer <span>12 years ago</span>
+            </h3>
+            <p>
+              Learning to code because believe that oneday it will work for me
+              IA Learning to code because believe that oneday it will work for
+              me IA Learning to code because believe that oneday it will work
+              for me IA Learning to code because believe that oneday it will
+              work for me IA Learning to code because believe that oneday it
+              will work for me IA Learning to code because believe that oneday
+              it will work for me IA Learning to code because believe that
+              oneday it will work for me IA Learning to code because believe
+              that oneday it will work for me IA
+            </p>
+            <div className="comment-action">
+              <img src={like} alt="" />
+              <span>14984</span>
+              <img src={dislike} alt="" />
+              <span>62368</span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
